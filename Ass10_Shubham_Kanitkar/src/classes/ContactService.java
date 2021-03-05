9ab808200ab87a4684a93d7474336d248f779900
skanitkar12345 <shubham.kanitkar16@vit.edu>
2021-03-05 20:11:29 +0530

    Added serialization and deserialization functions.

--- Ass10_Shubham_Kanitkar/src/classes/ContactService.java
+++ Ass10_Shubham_Kanitkar/src/classes/ContactService.java
@@ -1,14 +1,16 @@
 package classes;
 
 import java.util.*;
-import java.io.IOException;
+import java.io.*;
 import java.sql.*;
 //import db.CreateConnection;
 
 
 public class ContactService {
 	
-	//private static final String dbConfigFile = "src/database.properties";
+	private static final String dbConfigFile = "src/database.properties";
+	private static final String contactFile = "src/Contact.txt";
+	private static final String outFile = "D:/contactfile.txt";
 	
 	public void addContact(Contact contact, List<Contact> contacts) {
 		
@@ -95,7 +97,68 @@ public class ContactService {
 		
 	}
 	
-	public static void main(String[] args) throws IOException, SQLException {
+	public void sortContactsByName(List<Contact> contacts) {
+		contacts.sort(null);
+	}
+	
+	public void readContactsFromFile(List<Contact> contacts, String fName) throws IOException {
+		
+		FileInputStream fin = new FileInputStream(fName);
+		Scanner sc = new Scanner(fin);
+		String[] data;
+		
+		while (sc.hasNextLine()) {
+			Contact contact = new Contact();
+			List<String> numbers = new ArrayList<>();
+			data = sc.nextLine().split(",");
+			contact.setContactId(Integer.parseInt(data[0]));
+			contact.setContactName(data[1]);
+			contact.setContactEmailId(data[2]);
+			String[] contactNumbers = data[3].split("-");
+			for (String number : contactNumbers) {
+				numbers.add(number);
+			}
+			contact.setContactNumbers(numbers);
+			
+			contacts.add(contact);
+			
+		}
+		
+		sc.close();
+		fin.close();
+	}
+	
+	public void serialize(List<Contact> contacts, String fName) throws IOException {
+		File file = new File(fName);
+		FileOutputStream fout = new FileOutputStream(file);
+		ObjectOutputStream out = new ObjectOutputStream(fout);
+		
+		for (Contact contact : contacts) {
+			out.writeObject(contact);
+		}
+		
+		out.close();
+		fout.close();
+	}
+	
+	public List<Contact> deserialize(List<Contact> contacts,String fName) throws IOException, ClassNotFoundException {
+		
+		List<Contact> c = new ArrayList<>();
+		
+		File file = new File(fName);
+		FileInputStream fin = new FileInputStream(file);
+		ObjectInputStream in = new ObjectInputStream(fin);
+		
+		for(int i = 0; i<contacts.size(); i++)
+			c.add((Contact)in.readObject());
+		
+		in.close();
+		fin.close();
+		
+		return c;
+	}
+	
+	public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException {
 		
 		ContactService cs = new ContactService();
 		List<Contact> contacts = new ArrayList<>();
@@ -110,6 +173,8 @@ public class ContactService {
 			System.out.println("3. Search by name");
 			System.out.println("4. Search by number");
 			System.out.println("5. Add contact number to existing numbers");
+			System.out.println("6. Sort contacts according to name");
+			System.out.println("7. Add contacts from file");
 			System.out.println("------------------------------");
 			int n = sc.nextInt();
 			
@@ -170,6 +235,26 @@ public class ContactService {
 			        	System.out.println(e.getMessage());
 			        }
 			        break;
+			        
+			case 6: cs.sortContactsByName(contacts);
+			        for (Contact contact : contacts) {
+						System.out.println(contact.getContactName());
+					}
+			        
+			        break;
+			        
+			case 7: cs.readContactsFromFile(contacts, contactFile);
+			        break;
+			        
+			case 8: cs.serialize(contacts, outFile);
+			        System.out.println("Done");
+				    break;
+				    
+			case 9: List<Contact> contacts1 = cs.deserialize(contacts, outFile);
+			        for (Contact contact : contacts1) {
+						System.out.println(contact.getContactName());
+					}
+				    break;
 			   
 			default: System.exit(0);
 		
