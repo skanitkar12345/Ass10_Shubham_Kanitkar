97a870ba2c76eb89ae1c252d19cc7984e80edb66
skanitkar12345 <shubham.kanitkar16@vit.edu>
2021-03-05 17:26:21 +0530

    Added functions for searching contacts by number and adding new

--- Ass10_Shubham_Kanitkar/src/classes/ContactService.java
+++ Ass10_Shubham_Kanitkar/src/classes/ContactService.java
@@ -60,17 +60,56 @@ public class ContactService {
 		throw new ContactNotFoundException("Contact not found!");
 	}
 	
+	public List<String> searchContactByNumber(String contact, List<Contact> contacts) throws ContactNotFoundException{
+		
+		List<String> resultList = new ArrayList<>();
+		
+		for (Contact c : contacts) {
+			List<String> numbers = c.getContactNumbers();
+			
+			for (String number : numbers) {
+				if(number.contains(contact))
+					resultList.add(number);
+			}
+					
+		}
+		
+		if(resultList.isEmpty())
+			throw new ContactNotFoundException("No contact has any number which matches with input!");
+		
+		return resultList;
+	}
+	
+	public void addContactNumber(int id, String number, List<Contact> contacts) throws ContactNotFoundException{
+		
+		for (Contact contact : contacts) {
+			if (contact.getContactId()==id) {
+				List<String> numbers = contact.getContactNumbers();
+				numbers.add(number);
+				contact.setContactNumbers(numbers);
+				System.out.println("Success");
+				return;
+			} 
+		}
+		throw new ContactNotFoundException("Unsuccessful");
+		
+	}
+	
 	public static void main(String[] args) throws IOException, SQLException {
 		
 		ContactService cs = new ContactService();
 		List<Contact> contacts = new ArrayList<>();
 		Scanner sc = new Scanner(System.in);
 		
+		
 		while (true) {
 			System.out.println("------------------------------");
 			System.out.println("Enter choice ");
 			System.out.println("1. Add new contact");
 			System.out.println("2. Remove a contact");
+			System.out.println("3. Search by name");
+			System.out.println("4. Search by number");
+			System.out.println("5. Add contact number to existing numbers");
 			System.out.println("------------------------------");
 			int n = sc.nextInt();
 			
@@ -94,13 +133,43 @@ public class ContactService {
 			        sc.nextLine();
 			        String name = sc.nextLine();
 			        try {
-						cs.searchContacyByName(name, contacts);
+						Contact c1 = cs.searchContacyByName(name, contacts);
+						System.out.println("Following contact found");
+						System.out.println("------------------------------");
+						System.out.println(c1.getContactName());
 					} catch (Exception e) {
 						System.out.println(e.getMessage());
 					}
 			        
 			        break;
-			 
+			        
+			case 4: System.out.println("Enter contact number to be searched");
+	                sc.nextLine();
+	                String number = sc.nextLine();
+	                try {
+	                	List<String> numbers = cs.searchContactByNumber(number, contacts);
+	                	System.out.println("Following numbers found");
+	                	System.out.println("------------------------------");
+	                	for (String string : numbers) {
+							System.out.println(string);
+						}
+	                } catch (Exception e) {
+	                	System.out.println(e.getMessage());
+	                }
+	                
+	                break;
+			case 5: System.out.println("Enter contact id and new number");
+			        sc.nextLine();
+			        String newNumber = sc.nextLine();
+			        sc.nextLine();
+			        int id1 = sc.nextInt();
+			        try {
+			        	cs.addContactNumber(id1, newNumber, contacts);
+			        	
+			        } catch (Exception e) {
+			        	System.out.println(e.getMessage());
+			        }
+			        break;
 			   
 			default: System.exit(0);
 		
